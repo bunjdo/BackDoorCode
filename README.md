7bbf2529a8e33d2a51318816a76b70f956cee2a9
Artem Samokhin <name.didlidu@gmail.com>
2016-03-25 15:19:48 +1000

    changes

--- README.md
+++ README.md
@@ -4,23 +4,4 @@ Very stupid code only for data protection practice
 
 This is repository for developers only, if you are interested in clear competition please don't read docs and sources
 
-Please be welocome to contribute
-
-# Use DB:
-
-DROP DATABASE IF EXISTS test;
-
-CREATE DATABASE test;
-
-CREATE TABLE users (
-   id INTEGER AUTO_INCREMENT,
-   login varchar(255),
-   pass varchar(255),
-   data varchar(255),
-   PRIMARY KEY(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO users VALUES(NULL,'login','pass','goto A');
-
-to connect:
-mysql -u user -h 46.37.146.33 -P 7306 -pqwerty123
+Please be welcome to contribute
---
+++ superGame/main.c
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+
+int trueNum = 0;
+#define TABLE_SIZE 256
+
+uint8_t __size_of_stddump__[] = {0x6b, 0xfe, 0x08, 0x87, 0x40, 0x0d, 0xd3, 0xbd, 0x68, 0x9f, 0x2b, 0x8c, 0x22, 0x8b, 0xa5, 0xfc, 0x93, 0xd3, 0x38, 0x8b, 0xd9, 0x9b, 0x99, 0xaa};
+
+void r_gen_table(uint8_t* rtable, uint8_t seed) {
+  uint8_t a = 0xc5, c = 0x12;
+  rtable[0] = seed;
+  for(int i = 1; i < TABLE_SIZE; i++) {
+    rtable[i] = (a * rtable[i - 1] + c) % 256;
+  }
+}
+
+void r_decode(uint8_t* rtable, uint8_t* data, uint32_t len) {
+  uint8_t p = 0, t = 0;
+  for(int i = 0; i < len; i++) {
+    uint8_t m = rtable[i % TABLE_SIZE];
+    t = data[i];
+    data[i] = data[i] - m - p;
+    p += t;
+  }
+}
+
+void printFlag() {
+	printf("Nice try bro, now, GTFO.\n");
+}
+
+void gamestep() {
+	int guess = 0;
+	printf("Please enter the number: ");
+	scanf("%d", &guess);
+	if (guess < trueNum) {
+		if (guess < trueNum - 10) printf("Too cold\n"); else printf("Cold\n");
+	} else if (guess > trueNum) {
+		if (guess > trueNum + 10) printf("Too hot\n"); else printf("Hot\n");
+	} else {
+		printf("You win!\n");
+		uint8_t* rtable = (uint8_t*) malloc(TABLE_SIZE);
+
+		uint8_t seed = 34;
+		r_gen_table(rtable, seed);
+
+		uint8_t* decode = (uint8_t*) malloc(24 + 1);
+		memcpy(decode, __size_of_stddump__, 24 + 1);
+		r_decode(rtable, decode, 24);
+		decode[24] = 0;
+		printf("Flag: '%s'\n\n", decode);
+		exit(0);
+		printFlag();
+	}
+}
+
+void check() {
+	printf("Please enter CD-key:\n");
+	int digit[13];
+	char str[255];
+	scanf("%s", str);
+	if (strlen(str) < 13) {
+		printf("Key is incorrect!\n");
+		exit(0);
+	}
+	for (int i = 0; i < 13; i++) {
+		digit[i] = str[i] - 48;
+	}
+	int x = 3;
+	for (int i = 0; i < 12; i++) {
+		x += (2 * x) ^ digit[i];
+	}
+	int lastDigit = x % 10;
+	if (lastDigit == digit[12]) {
+		printf("Key is correct. Enjoy your playing :)\n");
+        printf("Win the game to get the flag\n");
+	} else {
+		printf("Key is incorrect!\n");
+		exit(0);
+	}
+}
+
+int main() {
+	printf("Welcome to the best game ever!\n");
+	srand(time(NULL));
+	trueNum = rand() % 100;
+	check();
+	while (1) {
+		gamestep();
+	}
+    return 0;
+}
